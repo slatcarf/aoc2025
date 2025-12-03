ed5c01b418ba12db85061fc04cdd122db7a82c6b
Julian Wasmeier <julian.wasmeier@outlook.com>
2025-12-03 01:52:13 +0100

    feat: day 1 part 2

--- day1.py
+++ day1.py
@@ -1,20 +1,32 @@
 f = open("input.txt")
 
+
+    
+
+
 current = 50;
 count = 0;
+
+def turn_dial(direction, amount):
+  global current
+  global count
+  for i in range(0, amount):
+    if(direction == "R"):
+        current = (current + 1) % 100
+    elif(direction == "L"):
+        current = (current - 1) % 100
+    if(current == 0):
+        count += 1
+
 for line in f:
     if(line == "\n"):
        break
     stripped = line.strip()
     direction = stripped[0]
     amount = stripped[1:]
+    turn_dial(direction, int(amount))
 
-    if(direction == "R"):
-        current = (current + int(amount)) % 100
-    elif(direction == "L"):
-        current = (current - int(amount)) % 100
-    if(current == 0):
-        count += 1
+  
 
 print(count)
 
