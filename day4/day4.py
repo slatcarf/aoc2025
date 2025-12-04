7a96307f3cc9db2e6a28a3b0e305d138ff4f7149
Julian Wasmeier <julian.wasmeier@outlook.com>
2025-12-04 12:08:50 +0100

    refactor: move boundary checking out of range call

--- day4/day4.py
+++ day4/day4.py
@@ -1,29 +1,27 @@
-file = open('input')
+file = open("input")
 
 
 file_arr = file.readlines()
 line_len = len(file_arr[0])
+file_len = len(file_arr)
 
 
 count = 0
 for i, line in enumerate(file_arr):
     for j, slot in enumerate(line):
         surrounding_count = 0
-        if(slot != '@'):
+        if slot != "@":
             continue
-     
-        for x in range(i if i == 0 else i - 1,i+1 if i == len(file_arr) - 1 else i + 2):
-            for y in range(j if j == 0 else j - 1,j+1 if j == line_len - 1 else j + 2):
+
+        for x in range(i - 1, i + 2):
+            for y in range(j - 1, j + 2):
                 # skip counting the inspected roll itself
-                if(x == i and y == j):
+                if x == i and y == j:
                     continue
 
-                if file_arr[x][y] == '@':
+                if 0 <= x < file_len and 0 <= y < line_len and file_arr[x][y] == "@":
                     surrounding_count += 1
         # print(slot,surrounding_count)
-        if surrounding_count < 4 and slot == '@':
-            count+=1
+        if surrounding_count < 4 and slot == "@":
+            count += 1
 print(count)
-                
-            
-            
